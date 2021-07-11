d5c6d4617e5d94ad4b1174d7633ff9ea8c175426
Moemad-711 <57157211+Moemad-711@users.noreply.github.com>
2021-07-11 21:55:21 +0200

    Update traffic_features_preprocessing_main.py

--- SingleIntersection/traffic_features_preprocessing_main.py
+++ SingleIntersection/traffic_features_preprocessing_main.py
@@ -5,7 +5,6 @@ import pandas as pd
 import os
 from mpi4py import MPI
 
-from PredictiveModel import PredictiveModel
 from utils import  read_data_from_xml
 
 
@@ -14,12 +13,16 @@ if __name__ == "__main__":
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
 
-    for index in range(80 + rank * 2, 80 + (rank * 2) + 2):
-        #print('hello from rank %i: '%(rank), index)
+    for index in range(rank * 2, (rank * 2) + 3):
         #reading data form files
+        if index >= 100:
+            break
         print('----- Readinding from data%i -----' % (index))
         file_name = 'data' + str(index) + '.xml'
         traffic_data = []
         traffic_data.append(read_data_from_xml(file_name,index))
+    
+    
+
 
 
