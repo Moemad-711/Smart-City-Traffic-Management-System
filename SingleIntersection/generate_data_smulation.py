7df838b555e178aad9d2df96d0a319f2d6968bd4
Moemad-711 <57157211+Moemad-711@users.noreply.github.com>
2021-07-12 13:03:21 +0200

    Update generate_data_smulation.py

--- SingleIntersection/generate_data_smulation.py
+++ SingleIntersection/generate_data_smulation.py
@@ -12,7 +12,7 @@ class Simulation:
         self._step = 0
 
       
-    def run(self, episode):
+    def run(self, episode, path):
        
         start_time = timeit.default_timer()
         self._TrafficGen.generate_routefile(seed=episode)
@@ -21,12 +21,13 @@ class Simulation:
         traci.start(self._sumo_cmd)
         
         print("Simulating...")
-        file_name ='./TrafficData/data'+str(episode)+'.xml'
+        file_name =path+'/data'+str(episode)+'.xml'
         
         with open(os.path.join(file_name),"w") as data:
             print("""<fcd-export xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://sumo.dlr.de/xsd/fcd_file.xsd">""",file=data)
 
             while self._step < self._max_steps:
+                #print('  Step-%i' % (self._step))
                 traci.simulationStep()
                 print('<timestep time="%d">' % (self._step),file=data)
                 car_list = traci.vehicle.getIDList()
@@ -46,7 +47,43 @@ class Simulation:
         
         traci.close()
         simulation_time = round(timeit.default_timer() - start_time, 1)
-   
-   
 
+        return simulation_time
+
+    def run_mpi(self, episode, path, rank, sumo_cmd):
+       
+        start_time = timeit.default_timer()
+        self._TrafficGen.generate_routefile_mpi(seed=episode, rank=rank)
+        self._step = 0
+
+        traci.start(sumo_cmd)
+        
+        print("Simulating...")
+        file_name =path+'/data'+str(episode)+'.xml'
+        
+        with open(os.path.join(file_name),"w") as data:
+            print("""<fcd-export xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://sumo.dlr.de/xsd/fcd_file.xsd">""",file=data)
+
+            while self._step < self._max_steps:
+                #print('  Step-%i' % (self._step))
+                traci.simulationStep()
+                print('<timestep time="%d">' % (self._step),file=data)
+                car_list = traci.vehicle.getIDList()
+                for car_id in car_list:
+                    #print(car_id)
+                    x,y = traci.vehicle.getPosition(car_id)
+                    angle=traci.vehicle.getAngle(car_id)                  
+                    vehicle_type=traci.vehicle.getTypeID(car_id)                  
+                    speed=traci.vehicle.getSpeed(car_id)
+                    lane_pos=traci.vehicle.getLanePosition(car_id)                   
+                    edge=traci.vehicle.getRoadID(car_id)
+                    lane_id=traci.vehicle.getLaneID(car_id)
+                    print('    <vehicle id="%s" x="%d" y="%d" angle="%d" type="%s" speed="%d" pos="%d" edge="%s" lane="%s" />' % (car_id, x, y, angle, vehicle_type, speed, lane_pos, edge, lane_id), file=data)
+                print('</timestep>',file=data)
+                self._step += 1
+            print('</fcd-export>',file=data)   
+        
+        traci.close()
+        simulation_time = round(timeit.default_timer() - start_time, 1)
+        
         return simulation_time
