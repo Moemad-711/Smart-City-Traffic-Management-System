9a71981fb25e8011d43aef58e4925c450469daa4
Moemad-711 <57157211+Moemad-711@users.noreply.github.com>
2021-07-12 13:30:27 +0200

    Update st_model_Training_main.py

--- SingleIntersection/st_model_Training_main.py
+++ SingleIntersection/st_model_Training_main.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
 
     config = import_st_model_train_configuration(config_file='st_model_training_setting.ini')
     path = set_train_path(config['models_path_name'])
-    
+    folder_num = config['traffic_feature_folder_num']
 
     adjacency_matrix = [[0, 750, 750, 750, 750],
                         [750, 0, 0, 0, 0],
@@ -32,7 +32,7 @@ if __name__ == "__main__":
         ### Reading Traffic information ###
         print('----- Reading From traffic features %i -----' % (index)) 
         file_name = 'traffic_features' + str(index) + '.csv'
-        traffic_features = pd.read_csv(os.path.join('TrafficFeatures',file_name))
+        traffic_features = pd.read_csv(os.path.join('TrafficFeatures','traffic_features_%i' %(folder_num),file_name))
 
         ### Splitting Data ###
         print(' spliting data...')
