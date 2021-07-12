357db554f760c422feb5b8bc1ba0341c303a9e9d
Moemad-711 <57157211+Moemad-711@users.noreply.github.com>
2021-07-12 13:30:15 +0200

    Update traffic_features_preprocessing_main.py

--- SingleIntersection/traffic_features_preprocessing_main.py
+++ SingleIntersection/traffic_features_preprocessing_main.py
@@ -12,13 +12,14 @@ if __name__ == "__main__":
 
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
+    dataset_num = 2
 
     for index in range(rank * 3, (rank * 3) + 3):
         #reading data form files
         if index >= 100:
             break
         print('----- Readinding from data%i -----' % (index))
-        path = os.path.join('TrafficData','traffic_data_%i' % (2))
+        path = os.path.join('TrafficData','traffic_data_%i' % (dataset_num))
         file_name = 'data' + str(index) + '.xml'
         traffic_data = []
         traffic_data.append(read_data_from_xml(os.path.join(path, file_name), index))
