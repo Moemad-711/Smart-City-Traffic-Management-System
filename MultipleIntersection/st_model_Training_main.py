7e3effc1d75185cd0b3a76994afad8ca7185b9c5
Moemad-711 <57157211+Moemad-711@users.noreply.github.com>
2021-07-19 01:09:20 +0200

    update

--- MultipleIntersection/st_model_Training_main.py
+++ MultipleIntersection/st_model_Training_main.py
@@ -22,14 +22,15 @@ if __name__ == "__main__":
                         [750, 0, 0, 0, 0],
                         [750, 0, 0, 0, 0],]
     
-    model = PredictiveModel(input_shape=(config['batch_size'],1,8), 
+    model = PredictiveModel(input_shape=(config['batch_size'],4,8), 
                             adjacency_matrix=adjacency_matrix, 
                             batch_size=config['batch_size'], 
                             prediction_steps=config['prediction_steps'])
                         
     start_time = timeit.default_timer()
+    total_episodes = 99
 
-    for index in range(99):
+    for index in range(total_episodes):
         ### Reading Traffic information ###
         print('----- Reading From traffic features %i -----' % (index)) 
         file_name = 'traffic_features' + str(index) + '.csv'
@@ -37,7 +38,7 @@ if __name__ == "__main__":
 
         ### Splitting Data ###
         print(' spliting data...')
-        x_train, y_train, x_val,  y_val = data_split(traffic_features.iloc[:,1:], 
+        x_train, y_train, x_val,  y_val = data_split(traffic_features.iloc[:,:], 
                                                      config['train_split'], 
                                                      config['batch_size'], 
                                                      config['prediction_steps'])
